70e90fde66cf2b382a696ae875c840780bb74730
Stanislav <anoshin.stanislav@yandex.ru>
2023-05-19 13:06:25 +0500

    HW7.1

---
+++ HW7.1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("введите количество строк");
+int rowVol = int.Parse(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = int.Parse(Console.ReadLine());
+double[,] numbers = new double[rowVol, columnsVol];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
